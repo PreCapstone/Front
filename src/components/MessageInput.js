671bc3f2c22919bd891bf4b71df3e4ebf9555268
이승언 <seungeon7878@gmail.com>
2024-11-16 04:25:12 +0900

    💄 전역 패딩 삭제, MessageInput 제목 주석처리

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Textarea = styled.textarea`
   width: 100%;
-  height: 100%;
+  height: 200px;
   padding: 10px;
   font-size: 16px;
   border: 1px solid #ccc;
@@ -24,7 +24,7 @@ const MessageInput = ({ message, setMessage }) => {
 
   return (
     <div>
-      <h2>메시지 입력</h2>
+      {/* <h2>메시지 입력</h2> */}
       <Textarea
         value={message}
         onChange={(e) => setMessage(e.target.value)}
