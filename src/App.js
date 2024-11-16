1922bd94163969f78d48b1dad7aa9f2cd0f37655
whikih34 <gitacc34@gmail.com>
2024-11-16 22:28:28 +0900

    🎨 로그인, 회원가입 페이지 구현 및 시작 페이지 변경
    로그인, 회원가입 페이지 구현 및 시작 페이지 변경

--- src/App.js
+++ src/App.js
@@ -5,15 +5,29 @@ import KeywordSelectionPage from './pages/KeywordSelectionPage';
 import RequirementsPage from './pages/RequirementsPage';
 import ImageEditingPage from './pages/ImageEditingPage';
 import SMSPage from './pages/MessageSendPage';
+import LoginPage from './pages/LoginPage';  // 로그인 페이지
+import SignupPage from './pages/SignupPage';  // 회원가입 페이지
 
 const App = () => {
-  const [activePage, setActivePage] = useState('MessageInput');
+  const [activePage, setActivePage] = useState('LoginPage');
   const [message, setMessage] = useState(''); // 메시지 상태
   const [imageHistory, setImageHistory] = useState([]);
   const [generatedImage, setGeneratedImage] = useState(null);
 
   const renderPage = () => {
     switch (activePage) {
+      case 'LoginPage':
+        return (
+          <LoginPage
+            setActivePage={setActivePage}
+          />
+        );
+      case 'SignupPage':
+        return (
+          <SignupPage
+            setActivePage={setActivePage}
+          />
+        ); 
       case 'MessageInput':
         return (
           <MessageInputPage
@@ -53,9 +67,11 @@ const App = () => {
     }
   };
 
+  const shouldShowMenuBar = activePage !== 'SMSPage' && activePage !== 'LoginPage' && activePage !== 'SignupPage';
+
   return (
     <div style={{ display: activePage === 'SMSPage' ? 'block' : 'flex', height: '100vh' }}>
-      {activePage !== 'SMSPage' && (
+      {shouldShowMenuBar && (
         <MenuBar activePage={activePage} setActivePage={setActivePage} />
       )}
       <div style={{ flexGrow: 1, padding: '0px' }}>{renderPage()}</div>
